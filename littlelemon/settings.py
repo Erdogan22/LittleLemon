51bd613e54e8d076ae528f8ada045595805a5f9b
Erdogan22 <mahmouderdoganba@gmail.com>
2025-04-24 00:24:30 +0200

    last of the day

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,11 +76,17 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': '',  # Add your MySQL password here
+        'HOST': 'localhost',
+        'PORT': '3306',  # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
     }
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Menu, Booking  # Import the actual model classes
 
-# Register your models here.
+# Register your models here
+admin.site.register(Menu)
+admin.site.register(Booking)
+
+#django dmin panel : usernme=Littlelemon, password=@littlelemon@
